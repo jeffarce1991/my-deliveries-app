f402a9b0a5ce0bcc2cf72122d8d73f54dfdd68bc
jeff <jeffandrew.s.arce@gmail.com>
2020-09-10 14:09:09 +0800

    Update onBindViewHolder.

--- app/src/main/java/com/jeff/deliveries/adapter/CustomAdapter.kt
+++ app/src/main/java/com/jeff/deliveries/adapter/CustomAdapter.kt
@@ -9,68 +9,70 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
-import com.jakewharton.picasso.OkHttp3Downloader
+import com.bumptech.glide.Glide
 import com.jeff.deliveries.R
 import com.jeff.deliveries.adapter.CustomAdapter.CustomViewHolder
-import com.jeff.deliveries.database.local.Photo
-import com.jeff.deliveries.databinding.CustomRowBinding
-import com.jeff.deliveries.main.detail.view.DetailsActivity
-import com.squareup.picasso.Picasso
+import com.jeff.deliveries.database.local.Delivery
+import com.jeff.deliveries.databinding.ItemDeliveryBinding
 
 internal class CustomAdapter(
     private val context: Context,
-    private val dataList: List<Photo>
+    private val dataList: List<Delivery>
 ) : RecyclerView.Adapter<CustomViewHolder>() {
 
-    internal inner class CustomViewHolder(binding: CustomRowBinding) :
+    internal inner class CustomViewHolder(binding: ItemDeliveryBinding) :
         ViewHolder(binding.root) {
         var itemLayout: ConstraintLayout = binding.itemLayout
-        var txtTitle: TextView = binding.customRowTitle
+        var from: TextView = binding.from
+        var to: TextView = binding.to
+        var price: TextView = binding.price
         val thumbnail: ImageView = binding.thumbnail
+        val favorite: TextView = binding.favorite
 
     }
 
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): CustomViewHolder {
-        val binding = DataBindingUtil.inflate<CustomRowBinding>(LayoutInflater.from(p0.context),
-            R.layout.custom_row,
+        val binding = DataBindingUtil.inflate<ItemDeliveryBinding>(
+            LayoutInflater.from(p0.context),
+            R.layout.item_delivery,
             p0,
-            false)
+            false
+        )
         return CustomViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
         val item = dataList[position]
-        holder.txtTitle.text = item.title
+        holder.from.text = String.format("From: ${item.route.start}")
+        holder.to.text = String.format("To: ${item.route.end}")
+        val deliveryFee: String = item.deliveryFee.replace("$", "")
+        val surcharge: String = item.deliveryFee.replace("$", "")
 
-        val builder = Picasso.Builder(context)
-        builder.downloader(OkHttp3Downloader(context))
-        builder.build().load(item.thumbnailUrl)
-            .placeholder(R.drawable.ic_launcher_background)
-            .error(R.drawable.ic_launcher_background)
-            .into(holder.thumbnail)
+        /*if (favoriteList[position].id == item.id) {
+            holder.favorite.show()
+        } else {
+            holder.favorite.hide()
+        }*/
 
-        /*
-        There was 1 cause:
-        java.io.FileNotFoundException(https://via.placeholder.com/600/92c952)
-        only on https://via.placeholder.com/
+        holder.price.text = String.format("$${surcharge.toFloat() + deliveryFee.toFloat()}")
 
         Glide
             .with(context)
-            .load(item.url)
+            .load(item.goodsPicture)
             .centerCrop()
             .placeholder(R.drawable.ic_launcher_background)
             .fallback(R.drawable.ic_launcher_background)
-            .into(holder.thumbnail)*/
+            .into(holder.thumbnail)
 
         holder.itemLayout.setOnClickListener {
-            val intent = DetailsActivity.getStartIntent(
+            /*val intent = DetailsActivity.getStartIntent(
                 context,
                 item.id,
                 item.title,
                 item.url,
                 item.thumbnailUrl
             )
-            context.startActivity(intent)
+            context.startActivity(intent)*/
         }
     }
 
