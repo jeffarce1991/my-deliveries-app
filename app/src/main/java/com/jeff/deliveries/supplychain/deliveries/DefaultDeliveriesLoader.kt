e564247816023b3f6fa24c6aa08138f837efac4f
jeff <jeffandrew.s.arce@gmail.com>
2020-09-10 19:11:09 +0800

    Add loadMoreDeliveries() in DefaultDeliveriesLoader.

--- app/src/main/java/com/jeff/deliveries/supplychain/deliveries/DefaultDeliveriesLoader.kt
+++ app/src/main/java/com/jeff/deliveries/supplychain/deliveries/DefaultDeliveriesLoader.kt
@@ -1,18 +1,18 @@
 package com.jeff.deliveries.supplychain.deliveries
 
 import com.jeff.deliveries.database.local.Delivery
-import com.jeff.deliveries.database.usecase.local.loader.PhotoLocalLoader
+import com.jeff.deliveries.database.usecase.local.loader.DeliveryLocalLoader
 import com.jeff.deliveries.database.usecase.local.saver.PhotoLocalSaver
 import com.jeff.deliveries.main.mapper.DeliveryDtoToDeliveryMapper
 import com.jeff.deliveries.webservices.internet.RxInternet
-import com.jeff.deliveries.webservices.usecase.loader.PhotoRemoteLoader
+import com.jeff.deliveries.webservices.usecase.loader.DeliveriesRemoteLoader
 import io.reactivex.Observable
 import io.reactivex.Single
 import javax.inject.Inject
 
 class DefaultDeliveriesLoader @Inject
-constructor(private val remoteLoader: PhotoRemoteLoader,
-            private val localLoader: PhotoLocalLoader,
+constructor(private val remoteLoader: DeliveriesRemoteLoader,
+            private val localLoader: DeliveryLocalLoader,
             private val localSaver: PhotoLocalSaver,
             private val rxInternet: RxInternet): DeliveriesLoader{
 
@@ -22,24 +22,35 @@ constructor(private val remoteLoader: PhotoRemoteLoader,
             .flatMapObservable { list -> Observable.fromIterable(list) }
             .flatMap(DeliveryDtoToDeliveryMapper())
             .toList()
-            //.flatMap { photos -> Single.fromObservable(localSaver.saveAll(photos)) }
-            .flatMap { photos -> Single.just(photos) }
+            .flatMap { Single.fromObservable(localSaver.saveAll(it)) }
+            .flatMap { Single.just(it) }
 
         //return session.activeAdministeredAccount()
         //.map { administeredAccount -> administeredAccount.tenantId }
         //.flatMap { tenantId -> remoteLoader.loadAvailableExpenses(tenantId) }
     }
 
-    override fun loadAllFromLocal(): Single<List<Delivery>> {
-        return rxInternet.notConnected()
-            .andThen(localLoader.loadAll())
-            /*.flatMap {
-                when {
-                    it.isEmpty() -> Single.error(
-                        Throwable(EmptyResultException()))
-                    else -> Single.just(it)
-                }
-            }*/
+    override fun loadMoreDeliveries(offset: Int): Single<List<Delivery>> {
+        return rxInternet.isConnected()
+            .andThen(remoteLoader.loadMoreDeliveries(offset))
+            .flatMapObservable { list -> Observable.fromIterable(list) }
+            .flatMap(DeliveryDtoToDeliveryMapper())
+            .toList()
+            .flatMap { Single.fromObservable(localSaver.saveAll(it)) }
+            .flatMap { Single.just(it) }
+
+        //return session.activeAdministeredAccount()
+        //.map { administeredAccount -> administeredAccount.tenantId }
+        //.flatMap { tenantId -> remoteLoader.loadAvailableExpenses(tenantId) }
+    }
+
+    override fun loadInitialLocally(): Single<List<Delivery>> {
+        return localLoader.loadInitial()
+            .flatMap { photos -> Single.just(photos)}
+    }
+
+    override fun loadMoreDeliveriesLocally(offset: Int): Single<List<Delivery>> {
+        return localLoader.loadMoreDeliveries(offset)
             .flatMap { photos -> Single.just(photos)}
     }
 
