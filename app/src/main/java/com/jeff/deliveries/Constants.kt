4edb1066d69e5e39b0563c9fa5e88a6d185c0a96
jeff <jeffandrew.s.arce@gmail.com>
2020-09-10 11:41:40 +0800

    Add LALAMOVE API base url.

--- app/src/main/java/com/jeff/deliveries/Constants.kt
+++ app/src/main/java/com/jeff/deliveries/Constants.kt
@@ -2,11 +2,6 @@ package com.jeff.deliveries
 
 class Constants private constructor() {
     object Gateways {
-        const val COVID19API = "https://api.covid19api.com"
-        const val JSONPLACEHOLDER = "https://jsonplaceholder.typicode.com"
-    }
-    object DaoExceptions {
-        const val ERROR_EMPTY_RESULT = "Empty results[] from DAO request"
-        const val ERROR_NULL_RESULT = "Null results[] from DAO request"
+        const val LALAMOVE = "https://mock-api-mobile.dev.lalamove.com/"
     }
 }
