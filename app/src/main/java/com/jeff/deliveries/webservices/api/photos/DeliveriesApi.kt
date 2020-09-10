0e11b53fe9907d63fef085a5f9437de27b333669
jeff <jeffandrew.s.arce@gmail.com>
2020-09-10 13:53:49 +0800

    Add DeliveriesApi.

--- app/src/main/java/com/jeff/deliveries/webservices/api/photos/DeliveriesApi.kt
+++ app/src/main/java/com/jeff/deliveries/webservices/api/photos/DeliveriesApi.kt
@@ -1,14 +1,13 @@
 package com.jeff.deliveries.webservices.api.photos
 
-import com.jeff.deliveries.webservices.dto.PhotoDto
+import com.jeff.deliveries.webservices.dto.DeliveryDto
 import io.reactivex.Single
 import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.Path
 
 interface DeliveriesApi {
 
     @GET("v2/deliveries?limit=20&offset=0")
-    fun loadDeliveries(): Single<Response<List<PhotoDto>>>
+    fun loadDeliveries(): Single<Response<List<DeliveryDto>>>
 
 }
